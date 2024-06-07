753e55482a70448af1c696551710bbc157268407
Ivanzr17 <ivanzafrarendal@gmail.com>
2024-06-07 21:25:29 +0200

    Proyecto finalizado

--- src/app.js
+++ src/app.js
@@ -9,3 +9,22 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+const numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+const suits = ["♦", "♥", "♠", "♣"];
+
+function random(len) {
+  return Math.floor(Math.random() * len);
+}
+
+const randomNumb = numbers[random(numbers.length)];
+const randomSuit = suits[random(suits.length)];
+
+document.querySelector(".suittop").classList.add(randomSuit);
+document.querySelector(".suitbot").classList.add(randomSuit);
+
+document.querySelector(".number").innerText = randomNumb;
+document.getElementById("top").innerText = randomSuit;
+document.getElementById("bot").innerText = document.getElementById(
+  "top"
+).innerText;
